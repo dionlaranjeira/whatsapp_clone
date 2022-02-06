dacd225a1ae3a6a5aabc5f15907b66adfb0ff09d
DION RIBEIRO LARANJEIRA <dionribeiro.rr@gmail.com>
2022-02-05 22:14:08 -0400

    Configurado Firebase para Android

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
@@ -8,11 +12,38 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  final String fullName = "Dion Ribeiro Laranjeira" ;
+  final String company = "CAER salvo no IOS";
+  final int age = 33;
+
+  CollectionReference users = FirebaseFirestore.instance.collection('users');
+
+  Future<void> addUser() {
+    // Call the user's CollectionReference to add a new user
+    return users
+        .add({
+      'full_name': fullName,
+      'company': company,
+      'age': age
+    })
+        .then((value) => print("User Added"))
+        .catchError((error) => print("Failed to add user: $error"));
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    addUser();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("WhatsApp"),
+        title: Text("WhatsApp clone"),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:whatsapp_clone/home.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(MaterialApp(
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const MaterialApp(
     home: Home(),
     debugShowCheckedModeBanner: false,
   ));
